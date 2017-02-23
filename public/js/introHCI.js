75e4a548134f8a79607fa700734883993279d679
Tamara Zubatiy <tzubatiy@ucsd.edu>
2017-02-23 10:41:24 -0800

    added min ab analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	
+	$(".likeCtr").click(function(){
+		ga("send","event", 'like', 'click')
+	})
 	// your code here
 }
+
